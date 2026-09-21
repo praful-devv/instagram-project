b81cad428ca867a664621bb3989a37ccff26868b
praful-devv <prafulanilwankhade3010@gmail.com>
2026-09-21 13:35:41 +0530

    post api done

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,25 +1,45 @@
-const postModel = require("../models/post.model")
-const ImageKit = require("@imagekit/nodejs")
-const {toFile} = require("@imagekit/nodejs")
+const postModel = require("../models/post.model");
+const ImageKit = require("@imagekit/nodejs");
+const { toFile } = require("@imagekit/nodejs");
 const imageKit = new ImageKit({
   privatekey: process.env.IMAGEKIT_PRIVATE_KEY,
 });
+const jwt = require("jsonwebtoken")
 
+const createPostController = async (req, res) => {
+  
+  const token = req.cookies.token
 
-const createPostController = async(req,res)=>{
-//     const { caption, createdBy } = req.body;
-    
+  if(!token){
+    return res.status(401).json({
+      message:"unauthorized Access"
+    })
+  }
 
-   const file = await imageKit.files.upload({
-    file:await toFile(Buffer.from(req.file.buffer),'file'),
-    fileName:'post'
-   })
+  let decode;
+  try {
+     decode = await jwt.verify(token, process.env.JWT_SECRETS);
+  } catch (error) {
+    return res.status(401).json({
+      message: "unauthorized Access",
+    });
+  }
 
-   res.status(200).json({
-    message:"post created"
-   })
+  const file = await imageKit.files.upload({
+    file: await toFile(Buffer.from(req.file.buffer), "file"),
+    fileName: "post",
+    folder:"post_img"
+  });
 
+  const post = await postModel.create({
+    caption:req.body.caption,
+    post_img:file.url,
+    createdBy:decode.id
+  })
 
-}
+  res.status(201).json({
+    message: "post created",
+  });
+};
 
-module.exports = {createPostController}
+module.exports = { createPostController };
